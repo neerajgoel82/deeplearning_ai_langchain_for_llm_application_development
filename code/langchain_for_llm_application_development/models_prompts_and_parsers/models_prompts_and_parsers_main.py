f04b32b653c6babcefbdba707545dc2cb0f60a9f
Neeraj Goel <negoel@neerajs-mbp.corp.adobe.com>
2024-09-30 16:00:39 -0700

    Setting the env variables properly

--- code/langchain_for_llm_application_development/models_prompts_and_parsers/models_prompts_and_parsers_main.py
+++ code/langchain_for_llm_application_development/models_prompts_and_parsers/models_prompts_and_parsers_main.py
@@ -7,6 +7,11 @@ from dotenv import load_dotenv, find_dotenv
 def main():
     _ = load_dotenv(find_dotenv())  # read local .env file
     openai.api_key = os.environ['OPENAI_API_KEY']
+    openai.api_base = os.environ['OPENAI_API_BASE']
+    openai.api_version = os.environ['OPENAI_API_VERSION']
+    openai.api_type = os.environ['OPENAI_API_TYPE']
+    openai.deployment_name = os.environ['OPENAI_DEPLOYMENT_NAME_GPT35']
+
     mpp_sample = MPPSample()
     mpp_sample.run()
 
