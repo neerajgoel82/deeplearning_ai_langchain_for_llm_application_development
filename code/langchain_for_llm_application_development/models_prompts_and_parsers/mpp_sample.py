bb1b75673d4902556b586177b6b54d4c66353ff1
Neeraj Goel <negoel@neerajs-mbp.corp.adobe.com>
2024-09-30 15:34:07 -0700

    Adding the implementation to test prompts

--- code/langchain_for_llm_application_development/models_prompts_and_parsers/mpp_sample.py
+++ code/langchain_for_llm_application_development/models_prompts_and_parsers/mpp_sample.py
@@ -1,6 +1,8 @@
 import openai
 import datetime
 from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
 
 class MPPSample:
     """
@@ -40,6 +42,68 @@ class MPPSample:
         else:
             self.llm_model = "gpt-3.5-turbo-0301"
 
+    def test_prompt_template(self):
+        template_string = """Translate the text \
+                        that is delimited by triple backticks \
+                        into a style that is {style}. \
+                        text: ```{text}```
+                        """
+
+
+        prompt_template = ChatPromptTemplate.from_template(template_string)
+        print(prompt_template.messages[0].prompt)
+        print(prompt_template.messages[0].prompt.input_variables)
+
+        customer_style = """American English \
+        in a calm and respectful tone
+        """
+
+        customer_email = """
+        Arrr, I be fuming that me blender lid \
+        flew off and splattered me kitchen walls \
+        with smoothie! And to make matters worse, \
+        the warranty don't cover the cost of \
+        cleaning up me kitchen. I need yer help \
+        right now, matey!
+        """
+
+        customer_messages = prompt_template.format_messages(
+            style=customer_style,
+            text=customer_email)
+
+        print(type(customer_messages))
+        print(type(customer_messages[0]))
+        print(customer_messages[0])
+
+        # Call the LLM to translate to the style of the customer message
+        customer_response = self.chat(customer_messages)
+        print(customer_response.content)
+
+        service_reply = """Hey there customer, \
+        the warranty does not cover \
+        cleaning expenses for your kitchen \
+        because it's your fault that \
+        you misused your blender \
+        by forgetting to put the lid on before \
+        starting the blender. \
+        Tough luck! See ya!
+        """
+
+        service_style_pirate = """\
+        a polite tone \
+        that speaks in English Pirate\
+        """
+
+        service_messages = prompt_template.format_messages(
+            style=service_style_pirate,
+            text=service_reply)
+
+        print(service_messages[0].content)
+
+        service_response = self.chat(service_messages)
+        print(service_response.content)
+
+
     def __init__(self):
         """
         Initializes the MPPSample class.
@@ -53,7 +117,4 @@ class MPPSample:
         self.chat = ChatOpenAI(temperature=0.0, model=self.llm_model)
 
     def run(self):
-        """
-        Prints the currently set language model.
-        """
-        print(self.llm_model)
+        self.test_prompt_template()
