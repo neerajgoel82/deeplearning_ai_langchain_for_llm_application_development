65f7eef0af33f285a627cfc32137a5e12693772c
Neeraj Goel <negoel@Neerajs-MacBook-Pro.local>
2024-09-30 12:24:39 -0700

    Adding some comments and added dependecy on langchain_openai

--- code/langchain_for_llm_application_development/models_prompts_and_parsers/mpp_sample.py
+++ code/langchain_for_llm_application_development/models_prompts_and_parsers/mpp_sample.py
@@ -1,10 +1,33 @@
 import openai
 import datetime
+from langchain_openai import ChatOpenAI
 
 class MPPSample:
+    """
+    A class to represent a sample model prompt and parser (MPP) using OpenAI's language models.
+
+    Attributes
+    ----------
+    llm_model : str
+        The language model to be used.
+
+    Methods
+    -------
+    choose_llm_model():
+        Chooses the appropriate language model based on the current date.
+    __init__():
+        Initializes the MPPSample class and sets the language model.
+    run():
+        Prints the currently set language model.
+    """
     llm_model = "gpt-3.5-turbo-0301"
 
     def choose_llm_model(self):
+        """
+        Chooses the appropriate language model based on the current date.
+        If the current date is after June 12, 2024, sets the model to "gpt-3.5-turbo".
+        Otherwise, sets the model to "gpt-3.5-turbo-0301".
+        """
         # Get the current date
         current_date = datetime.datetime.now().date()
 
@@ -18,7 +41,19 @@ class MPPSample:
             self.llm_model = "gpt-3.5-turbo-0301"
 
     def __init__(self):
+        """
+        Initializes the MPPSample class.
+        Calls the choose_llm_model method to set the language model.
+        Initializes the ChatOpenAI instance with the chosen model and a temperature of 0.0.
+        """
         self.choose_llm_model()
 
+        # To control the randomness and creativity of the generated
+        # text by an LLM, use temperature = 0.0
+        self.chat = ChatOpenAI(temperature=0.0, model=self.llm_model)
+
     def run(self):
+        """
+        Prints the currently set language model.
+        """
         print(self.llm_model)
